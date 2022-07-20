fff344ce8d7e353a468c35570b226ee7d612aa76
Rick Kersmarki <rkersmarki@gmail.com>
2022-07-19 22:35:08 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner;
+
+internal class Building
+{
+    public Building(string address, string designer)
+    {
+        _address = address;
+        _designer = designer;
+    }
+
+
+    private string _designer;
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public int Stories { get; set; }
+    public double Width { get; set; }   
+    public double Height { get; set; }  
+    public double Volume => Width * Height * (3*Stories);
+
+    public void Construct()
+    {
+        _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase(string buyer)
+    {
+        _owner = buyer; 
+    }
+
+    internal string DisplayInfo()
+    {
+        return $@"
+{_address}
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space
+";
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,45 @@
+﻿using UrbanPlanner;
+
+
+Building LAndC = new Building("123 Union St.", "Terry Pluckens")
+{
+    Stories = 70,
+    Width = 45.25,
+    Height = 900.33
+
+};
+Building Batman = new Building("222 Commerce St.", "Quint Horton")
+{
+    Stories = 80,
+    Width = 48.75,
+    Height = 989.40
+
+}; 
+Building BridgeStone = new Building("445 Broadway", "Solomon Gumball")
+{
+    Stories = 15,
+    Width = 400.25,
+    Height = 300.86
+
+};
+
+LAndC.Construct();
+Batman.Construct();
+BridgeStone.Construct();    
+
+LAndC.Purchase("Dorf Goldman");
+Batman.Purchase("Petunia Longfloss");
+BridgeStone.Purchase("Vigo Vesputin");
+
+List<Building> buildings = new List<Building>();
+
+buildings.Add(LAndC);
+buildings.Add(Batman);  
+buildings.Add(BridgeStone);
+
+foreach (Building tower in buildings)
+{
+    Console.WriteLine(tower.DisplayInfo());
+}
+
+Console.ReadLine();
